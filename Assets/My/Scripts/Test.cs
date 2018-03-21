13ed6a12bac3574b00a2c28fe410f68ee960d33f
RoundedKnights <943670930@qq.com>
2018-03-21 11:12:40 +0800

    first
    第一次提交

---
+++ Assets/My/Scripts/Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//运行测试程序
+		DBManager.Instance ().DemoTest ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
